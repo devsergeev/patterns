3c8eec4acadb453b3199086344d870a06b9ba019
devsergeev <sergeev.vasily.v@gmail.com>
2022-07-09 00:00:42 +0300

    creational: singleton

---
+++ Creational/Singleton/Singleton.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patterns\Creational\Singleton;
+
+use Exception;
+
+final class Singleton
+{
+    private static ?Singleton $instance = null;
+
+    public static function getInstance(): Singleton
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * Создавать через new запрещаем
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * Клонировать запрещаем
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * Дессериализацию запрещаем
+     * @throws Exception
+     */
+    public function __wakeup()
+    {
+        throw new Exception("Невозможно десериализовать синглтон");
+    }
+}
